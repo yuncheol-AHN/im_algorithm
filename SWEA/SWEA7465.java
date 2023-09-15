8fd61005005bc9c073700fba8d382376b1fccdb1
yuncheol-AHN <anssd1@naver.com>
2023-09-15 17:55:51 +0900

    SWEA7465 창용이

---
+++ SWEA/SWEA7465.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class SWEA7465 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		System.setIn(new FileInputStream("in"));
+		java.util.Scanner sc = new java.util.Scanner(new InputStreamReader(System.in));
+		
+		int T = sc.nextInt();
+		
+		for (int xx = 1; xx <= T; xx++) {
+			
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			for (int i = 0; i < M; i++) {
+				
+				int A = sc.nextInt();
+				int B = sc.nextInt();
+				
+				List<List<Integer>> list = new ArrayList<>();
+				
+				for (List<Integer> items: list) {
+					if (items.contains(items)) {
+						
+					}
+				}
+			}
+		}
+	}
+
+}
