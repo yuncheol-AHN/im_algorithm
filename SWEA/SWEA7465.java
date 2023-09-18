615f51037c2d41573073367e3e5fda8e33329fe0
yuncheol-AHN <anssd1@naver.com>
2023-09-18 17:48:51 +0900

    SWEA 창용마을 무리 해결

--- SWEA/SWEA7465.java
+++ SWEA/SWEA7465.java
@@ -2,9 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class SWEA7465 {
-
+	static int[] parents;
+	static int N;
+	
+	static void make() {
+		parents = new int[N];
+		for (int i = 0; i < N; i++) {
+			parents[i] = i;
+		}
+	}
+	static int find(int x) {
+		if (x == parents[x]) {
+			return x;
+		}
+		
+		return parents[x] = find(parents[x]);
+	}
+	static boolean union(int x, int y) {
+		int xRoot = find(x);
+		int yRoot = find(y);
+		
+		if (xRoot == yRoot) return false;
+		
+		parents[yRoot] = xRoot;
+		return true;
+	}
+	static void setting() {
+		for (int i = 0; i < N; i++) {
+			parents[i] = find(i);
+		}
+	}
+	
+	static void print() {
+		for (int e: parents) {
+			System.out.print(e + " ");
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		
 		System.setIn(new FileInputStream("in"));
 		java.util.Scanner sc = new java.util.Scanner(new InputStreamReader(System.in));
@@ -13,22 +49,31 @@ public class SWEA7465 {
 		
 		for (int xx = 1; xx <= T; xx++) {
 			
-			int N = sc.nextInt();
+			N = sc.nextInt();
 			int M = sc.nextInt();
+			
+			boolean[] chk = new boolean[N];
+			int cnt = 0;
+			
+			make();
 			
 			for (int i = 0; i < M; i++) {
 				
-				int A = sc.nextInt();
-				int B = sc.nextInt();
+				int x = sc.nextInt() - 1;
+				int y = sc.nextInt() - 1;
 				
-				List<List<Integer>> list = new ArrayList<>();
-				
-				for (List<Integer> items: list) {
-					if (items.contains(items)) {
-						
-					}
+				union(x, y);
+			}
+			
+			chk = new boolean[N];
+			for (int i = 0; i < N; i++) {
+				if (chk[find(i)] == false) {
+					chk[find(i)] = true;
+					cnt += 1;
 				}
 			}
+			
+			System.out.printf("#%d %d\n", xx, cnt);
 		}
 	}
 
