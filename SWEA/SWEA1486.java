d22f18b4de6cba0928c2048798e2e09e8a582d7c
안윤철 <anssd1@naver.com>
2023-09-25 17:27:51 +0900

    SWEA 장훈이의 높은 선반

---
+++ SWEA/SWEA1486.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA1486 {
+	static int[] arr;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for (int xx = 1; xx <= T; xx++) {
+			
+			st = new StringTokenizer(br.readLine().trim());
+			int N = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			arr = new int[N];
+			
+			st = new StringTokenizer(br.readLine().trim());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int min = Integer.MAX_VALUE;
+			
+			while () {
+				
+			}
+		}
+		
+		private static void recursive(int[] sel, )
+	}
+}
